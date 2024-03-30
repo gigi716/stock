3613f55ac403d9994214bc1ce91d6d785cd5a25e
younggwang Ji <gigi716@naver.com>
2024-03-30 19:10:21 +0900

    autocomplete

--- src/main/java/com/zerobase/stock/service/CompanyService.java
+++ src/main/java/com/zerobase/stock/service/CompanyService.java
@@ -8,7 +8,10 @@ import com.zerobase.stock.persist.repository.CompanyRepository;
 import com.zerobase.stock.persist.repository.DividendRepository;
 import com.zerobase.stock.scraper.Scraper;
 import lombok.AllArgsConstructor;
+import org.apache.commons.collections4.Trie;
+import org.apache.commons.collections4.trie.PatriciaTrie;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -21,6 +24,7 @@ import java.util.stream.Collectors;
 public class CompanyService {
 
 
+    private final Trie trie;
     private final Scraper yahooFinanceScraper;
 
     private final CompanyRepository companyRepository;
@@ -55,5 +59,27 @@ public class CompanyService {
 
         this.dividendRepository.saveAll(dividendEntities);
         return company;
+    }
+
+    public List<String> getCompanyNamesByKeyword(String keyword){
+        Pageable limit = PageRequest.of(0,10);
+        Page<CompanyEntity> companyEntities = this.companyRepository.findByNameStartingWithIgnoreCase(keyword, limit);
+        return companyEntities.stream()
+                .map(e -> e.getName())
+                .collect(Collectors.toList());
+    }
+
+    public void addAutocompleteKeyword(String keyword){
+        this.trie.put(keyword,null);
+    }
+
+    public List<String> autocomplete(String keyword){
+       return (List<String>) this.trie.prefixMap(keyword).keySet()
+                .stream()
+                .collect(Collectors.toList());
+    }
+
+    public void deleteAutocompleteKeyword(String keyword){
+        this.trie.remove(keyword);
     }
 }
