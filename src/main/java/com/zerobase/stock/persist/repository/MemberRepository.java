7d7cc691279b2a4fb8be62df65d69172b5d74712
younggwang Ji <gigi716@naver.com>
2024-03-31 21:21:48 +0900

    security with token

---
+++ src/main/java/com/zerobase/stock/persist/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package com.zerobase.stock.persist.repository;
+
+import com.zerobase.stock.model.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+
+    Optional<MemberEntity> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}
